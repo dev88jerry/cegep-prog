1487dc073bcc1e1afe613d1d28416f89591c9489
dev88jerry <70291890+dev88jerry@users.noreply.github.com>
2020-09-03 20:05:45 -0400

    updated do while part
    also completed print 2# after deci

--- ConversionTemperature.java
+++ ConversionTemperature.java
@@ -20,36 +20,35 @@ public class ConversionTemperature {
         // TODO code application logic here
         System.out.println("Convertisseur degres celsius et degres fahrenheit");
         System.out.println("-------------------------------------------------");
-        System.out.println("Choisissez le mode de conversion : ");
-        System.out.println("1 - Convertisseur Celsius - Fahrenheit");
-        System.out.println("2 - Convertisseur Fahrenheit - Celsius");
-        Scanner inp1 = new Scanner(System.in);        
-        int nbl = inp1.nextInt();
-        
-        System.out.println("Temperature a convertir : ");
-        Scanner tIN = new Scanner(System.in);
-        double temp = tIN.nextDouble();
-        double conv;
-        
-        if(nbl == 1){
-            conv = ((temp * 9.0)/5.0) + 32.0;
-            System.out.println(temp + " C correspond a : " + conv + " F.");
-            
-        }
-        else if(nbl == 2){
-            conv = (temp - 32.0) * 5.0 / 9.0;
-            System.out.println(temp + " F correspond a : " + conv + " C.");
-        }
-        
-        //TODO for lab
-        /*
-        write do while/while loop to allow additional entries
-        Souhaitez-vous convertir une autre tempterature ? (o/n)
-        Print only 2 # after decimal
-        */
-        
-       
-        
+        char autreT;
+        do {
+            System.out.println("Choisissez le mode de conversion : ");
+            System.out.println("1 - Convertisseur Celsius - Fahrenheit");
+            System.out.println("2 - Convertisseur Fahrenheit - Celsius");
+            Scanner inp1 = new Scanner(System.in);
+            int nbl = inp1.nextInt();
+
+            System.out.println("Temperature a convertir : ");
+            Scanner tIN = new Scanner(System.in);
+            double temp = tIN.nextDouble();
+            double conv;
+
+            if (nbl == 1) {
+                conv = ((temp * 9.0) / 5.0) + 32.0;
+                String strDouble = String.format("%.2f", conv);
+                System.out.println(temp + " C correspond a : " + strDouble + " F.");
+
+            } else if (nbl == 2) {
+                conv = (temp - 32.0) * 5.0 / 9.0;
+                String strDouble = String.format("%.2f", conv);
+                System.out.println(temp + " F correspond a : " + strDouble + " C.");
+            }
+
+            System.out.println("Souhaitez-vous convertir une autre tempterature ? (o/n)");
+            Scanner nextT = new Scanner(System.in);
+            autreT = nextT.next().charAt(0);
+
+        } while (autreT != 'n');
     }
-    
+
 }
